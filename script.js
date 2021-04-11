da7455a20fe1a3a02b49260d4e4a8cc7316abc5f
RickyAnchores <ricky23anchores@gmail.com>
2021-04-11 21:03:49 +0100

    completed

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const songs = ['Paris', 'Jumpsuit', 'Truelove']
 
 //Keep Track of songs
 
-let songIndex = 2;
+let songIndex = 1;
 
 //Initially Load song info DOM
 loadSong(songs[songIndex])
@@ -46,9 +46,9 @@ function pauseSong() {
 }
 
 function prevSong(){
-    songIndex --
+    songIndex--
 
-    if( song < 0 ) {
+    if( songIndex < 0 ) {
         songIndex = songs.length - 1 
     }
 
@@ -58,9 +58,30 @@ function prevSong(){
 }
 
 function nextSong(){
+    songIndex++
 
+    if ( songIndex > songs.length -1){
+        songIndex = 0
+    }
+
+    loadSong(songs[songIndex])
+
+    playSong()
 }
 
+function updateProgress(e){
+    const {duration, currentTime} = e.srcElement
+    const progressPercent = (currentTime / duration) * 100
+    progress.style.width =`${progressPercent}%`
+}
+
+function setProgress(e){
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime = (clickX / width ) * duration
+}
 // Event Listeners
 
 playBtn.addEventListener('click', () => {
@@ -76,3 +97,9 @@ playBtn.addEventListener('click', () => {
 // Change song events
 prevBtn.addEventListener('click', prevSong)
 nextBtn.addEventListener('click', nextSong)
+
+audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong)
