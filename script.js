eec8b26d66569ba62d28e955710606a06f05fb01
RickyAnchores <ricky23anchores@gmail.com>
2021-04-11 20:44:44 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,78 @@
+const musicContainer = document.querySelector('.music-container');
+const playBtn = document.querySelector('#play');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+const audio = document.querySelector('#audio');
+const progress = document.querySelector('.progress');
+const progressContainer = document.querySelector('.progress-container');
+const title = document.querySelector('#title');
+const cover = document.querySelector('#cover');
+
+
+//Song Titles
+
+const songs = ['Paris', 'Jumpsuit', 'Truelove']
+
+//Keep Track of songs
+
+let songIndex = 2;
+
+//Initially Load song info DOM
+loadSong(songs[songIndex])
+
+//Update song details
+
+function loadSong(song){
+    title.innerText = song
+    audio.src = `music/${song}.mp3`
+    cover.src = `images/${song}.jpeg`
+}
+
+
+function playSong(){
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play')
+    playBtn.querySelector('i.fas').classList.add('fa-pause')
+
+    audio.play()
+}
+
+function pauseSong() {
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play')
+    playBtn.querySelector('i.fas').classList.remove('fa-pause')
+
+    audio.pause()
+}
+
+function prevSong(){
+    songIndex--
+
+    if( song < 0 ) {
+        songIndex = songs.length - 1 
+    }
+
+    loadSong(songs[songIndex])
+
+    playSong();
+}
+
+function nextSong(){
+
+}
+
+// Event Listeners
+
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play')
+
+    if(isPlaying){
+        pauseSong()
+    } else {
+        playSong()
+    }
+})
+
+// Change song events
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
